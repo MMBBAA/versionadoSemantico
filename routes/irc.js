661d2167e1d2d76405c83fd668429a1919955f6c
a-bedmarmto@outlook.com <a-bedmarmto@outlook.com>
2020-05-07 04:19:09 +0200

    trabajando en version 2

--- routes/irc.js
+++ routes/irc.js
@@ -1,9 +1,14 @@
 var irc = require('../lib/irc'); //var bmi = require('../lib/bmi');
+
+const patient = new patientClass(160, 72); //creación del objeto
 module.exports = function(req, res, next) {
     return res.json({
         result: {
-            bmi: irc.getIndexIRC(req.weight, req.height, req.unit), // bmi: bmi.getIndex(req.weight, req.height, req.unit),
-            description: irc.getDescriptionIRC(irc.getIndexIRC(req.weight, req.height, req.unit), req.lang) //description: bmi.getDescription(bmi.getIndex(req.weight, req.height, req.unit), req.lang)
+            bmi: patient.getIndexIRC(req.height, req, weight, req.unit), //con objeto
+            // bmi: irc.getIndexIRC(req.weight, req.height, req.unit), // bmi: bmi.getIndex(req.weight, req.height, req.unit),
+            description: patientClass.getDescriptionIRC(patient.getIndexIRC(height, weight), lang) //con metodo estatico y objeto
+                // description: irc.getDescriptionIRC(irc.getIndexIRC(req.weight, req.height, req.unit), req.lang) //description: bmi.getDescription(bmi.getIndex(req.weight, req.height, req.unit), req.lang)
+
         }
     });
 };
